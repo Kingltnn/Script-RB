786fdf66fb0bec80e77ceae5257f2ca7dfaba071
Kingltnn <108787320+Kingltnn@users.noreply.github.com>
2025-04-26 10:35:52 +0700

    Create ZapPetGo.lua

---
+++ ZapPetGo.lua
@@ -0,0 +1,112 @@
+--Pets GO!
+_G.ZapSettings = {
+	["PetsGo - AutoArcadeTapToggle"] = true, 
+	["PetsGo - AutoRollArcadeEggToggle"] = true, 
+	["PetsGo - AutoArcadeRebirthTypeDropdown"] = "50", -- Options = {"1", "5", "10", "20", "25", "50"}
+	["PetsGo - AutoArcadeRebirthToggle"] = true, 
+	["PetsGo - AutoClaimArcadeQuestRewardsToggle"] = true,
+	["PetsGo - AutoOpenArcadeChestToggle"] = true,
+	["PetsGo - AutoCombineArcadeKeyToggle"] = true,
+	["PetsGo - AutoUseArcadePotionsTableMultiDropdown"] = {"Arcade Click Potion", "Arcade Luck Potion", "Arcade Key Potion", "Arcade God Boost"}, -- Options = {"Arcade Click Potion", "Arcade Luck Potion", "Arcade Key Potion", "Arcade God Boost"}
+	["PetsGo - AutoUseArcadePotionsToggle"] = true, 
+	["PetsGo - NewTitanicPetsWebhookInput"] = {"https://discord.com/api/webhooks/1365527586360262676/F6jUz2m6xIO2JBMkjeFuzMxb8Vl6SN3sfrZJpwYXjGGvq3-gS2uEbc50cGc73CWTXEr8"}, 
+	["PetsGo - NewTitanicPetsToggle"] = true, 
+	["PetsGo - NewHugePetsWebhookInput"] = {"https://discord.com/api/webhooks/1365527586360262676/F6jUz2m6xIO2JBMkjeFuzMxb8Vl6SN3sfrZJpwYXjGGvq3-gS2uEbc50cGc73CWTXEr8"}, 
+	["PetsGo - NewHugePetsToggle"] = true, 
+	["PetsGo - NewPetsMinimumDifficultyInput"] = {"https://discord.com/api/webhooks/1365527586360262676/F6jUz2m6xIO2JBMkjeFuzMxb8Vl6SN3sfrZJpwYXjGGvq3-gS2uEbc50cGc73CWTXEr8"}, 
+	["PetsGo - NewPetsWebhookInput"] = {"https://discord.com/api/webhooks/1365527586360262676/F6jUz2m6xIO2JBMkjeFuzMxb8Vl6SN3sfrZJpwYXjGGvq3-gS2uEbc50cGc73CWTXEr8"}, 
+	["PetsGo - NewPetsToggle"] = true, 
+	["PetsGo - DisableBreakablesModelsToggle"] = true, 
+	["PetsGo - DisablePetsRenderingToggle"] = true,, 
+	["PetsGo - DisableEffectsRenderingToggle"] = true, 
+	["PetsGo - DisableConfettiRenderingToggle"] = true, 
+	["PetsGo - DeleteUnnecessaryPartsToggle"] = true, 
+	["PetsGo - InfinitePetSpeedToggle"] = true, 
+	["PetsGo - AutoFarmToggle"] = true, 
+	["PetsGo - AutoCollectToggle"] = true, 
+	["PetsGo - AutoCollectHiddensToggle"] = true, 
+	["PetsGo - AutoDigRedCrossToggle"] = true, 
+	["PetsGo - AutoPopFlyingGiftsToggle"] = true, 
+	["PetsGo - AutoFishingToggle"] = true, 
+	["PetsGo - BypassFishingPositionToggle"] = false, 
+	["PetsGo - AutoBuyFishingMerchantToggle"] = true, 
+	["PetsGo - AutoUseFishingBaitTableMultiDropdown"] = {"Basic Fishing Bait", "Rare Fishing Bait", "Epic Fishing Bait", "Legendary Fishing Bait",}, -- Options = {"Basic Fishing Bait", "Rare Fishing Bait", "Epic Fishing Bait", "Legendary Fishing Bait", "Exclusive Fishing Bait"}
+	["PetsGo - AutoUseFishingBaitToggle"] = true, 
+	["PetsGo - TreasureChestTableMultiDropdown"] = {"Basic Treasure Chest", "Rare Treasure Chest", "Epic Treasure Chest", "Legendary Treasure Chest",}, -- Options = {"Basic Treasure Chest", "Rare Treasure Chest", "Epic Treasure Chest", "Legendary Treasure Chest", "Exclusive Treasure Chest"}
+	["PetsGo - AutoOpenTreasureChestToggle"] = true, 
+	["PetsGo - AutoIceFishingToggle"] = false, 
+	["PetsGo - BypassIceFishingPositionToggle"] = false, 
+	["PetsGo - AutoBuyIceFishingMerchantToggle"] = false, 
+	["PetsGo - AutoUseFrozenFishingBaitToggle"] = false, 
+	["PetsGo - AutoOpenFrozenTreasureChestToggle"] = false, 
+	["PetsGo - AutoDarkIceFishingToggle"] = true, 
+	["PetsGo - BypassDarkIceFishingPositionToggle"] = true, 
+	["PetsGo - AutoOpenAbyssalTreasureChestToggle"] = true, 
+	["PetsGo - AutoKrakenFishingToggle"] = true, 
+	["PetsGo - BypassKrakenFishingPositionToggle"] = true, 
+	["PetsGo - AutoClaimFishermanContentsToggle"] = true, 
+	["PetsGo - AutoBuyFishingBoatTycoonUpgradesToggle"] = true, 
+	["PetsGo - AutoMiningToggle"] = true, , 
+	["PetsGo - BypassMiningPositionToggle"] = true,  
+	["PetsGo - FarmMagmaOreToggle"] = true,  
+	["PetsGo - AutoBuyMiningMerchantToggle"] = false, 
+	["PetsGo - MiningChestTableMultiDropdown"] = {"Basic Mining Chest", "Rare Mining Chest", "Epic Mining Chest", "Legendary Mining Chest",}, -- Options = {"Basic Mining Chest", "Rare Mining Chest", "Epic Mining Chest", "Legendary Mining Chest", "Celestial Mining Chest", "Runic Mining Chest", "Titanic Mining Chest"}
+	["PetsGo - AutoOpenMiningChestToggle"] = true, 
+	["PetsGo - AutoUseMiningScrollTableMultiDropdown"] = {"Mining Damage Scroll", "Mining Loot Scroll", "Mining Speed Scroll", "Magma Scroll"}, -- Options = {"Mining Damage Scroll", "Mining Loot Scroll", "Mining Speed Scroll", "Magma Scroll"}
+	["PetsGo - AutoUseMiningScrollToggle"] = true, 
+	["PetsGo - AutoTitanicVaultToggle"] = true,  
+	["PetsGo - AutoThievingToggle"] = true, 
+	["PetsGo - AutoThievingStallDropdown"] = "Royal Stall", -- Options = {"Busted Shack", "Wooden Stall", "Stone Stall", "Marble Stall", "Bronze Stall", "Advanced Stall", "Golden Stall", "Royal Stall"}
+	["PetsGo - BypassThievingPositionToggle"] = true, 
+	["PetsGo - AutoBuyThievingMerchantToggle"] = true,  
+	["PetsGo - ThievingChestTableMultiDropdown"] = {"Basic Thieving Chest", "Rare Thieving Chest", "Epic Thieving Chest", "Legendary Thieving Chest",}, -- Options = {"Basic Thieving Chest", "Rare Thieving Chest", "Epic Thieving Chest", "Legendary Thieving Chest", "Celestial Thieving Chest", "Runic Thieving Chest", "Titanic Thieving Chest"}
+	["PetsGo - AutoOpenThievingChestToggle"] = true, 
+	["PetsGo - AutoUseThievingScrollTableMultiDropdown"] = {"Thieving Success Scroll", "Thieving Luck Scroll", "Thieving Speed Scroll"}, -- Options = {"Thieving Success Scroll", "Thieving Luck Scroll", "Thieving Speed Scroll"}
+	["PetsGo - AutoUseThievingScrollToggle"] = true, 
+	["PetsGo - AutoRollToggle"] = true, 
+	["PetsGo - AutoClaimBonusRollToggle"] = true, 
+	["PetsGo - DicePotionTableMultiDropdown"] = {}, -- Options = {"Golden Dice Potion", "Rainbow Dice Potion", "Blazing Dice Potion"}
+	["PetsGo - AutoUseDicePotionToggle"] = false, 
+	["PetsGo - AutoUseInstantLuckPotionTypeDropdown"] = "Instant Luck Potion 1", -- Options = {"Instant Luck Potion 1", "Instant Luck Potion 2", "Instant Luck Potion 3", "Instant Luck Potion 4"}
+	["PetsGo - AutoUseInstantLuckPotionWhenDropdown"] = "Golden Rainbow or Blazing", -- Options = {"Golden, Rainbow or Blazing", "Golden", "Rainbow", "Blazing", "Golden and Rainbow", "Golden, Rainbow and Blazing"}
+	["PetsGo - AutoUseInstantLuckPotionToggle"] = false, 
+	["PetsGo - AutoMailPetsAccountUsernameInput"] = false, 
+	["PetsGo - AutoMailPetsToggle"] = false, 
+	["PetsGo - AutoMailItemsAccountUsernameInput"] = false, 
+	["PetsGo - AutoMailItemsTableMultiDropdown"] = {}, -- Options = {"Crystal Key Lower Half", "Crystal Key Upper Half", "Crystal Key", "Secret Key", "Legendary Fishing Bait", "Exclusive Fishing Bait", "Corrupted Huge Bait", "Legendary Treasure Chest", "Exclusive Treasure Chest", "Abyssal Treasure Chest", "Golden Fishing Rod", "Diamond Fishing Rod", "Frozen Fishing Rod", "Corrupted Fishing Rod", "Instant Luck Potion 4", "Desert God Potion", "Thieving God Potion", "Heartbreak God Potion", "Jelly God Potion", "Autumn God Potion", "Winter God Potion", "Holiday God Potion", "God Potion", "Huge Egg", "Hype Egg", "Winter Egg", "Naughty Egg", "Nice Egg", "Holiday Egg", "Cupid Egg", "Heartbreak Egg", "Titanic Gift Bag", "Naughty Gift", "Nice Gift", "Holiday Gift", "Santa's Scroll", "Ice Fishing Scroll", "Holiday Damage Scroll"}
+	["PetsGo - AutoMailItemsToggle"] = false, 
+	["PetsGo - AutoClaimPlayTimeRewardsToggle"] = true, 
+	["PetsGo - AutoBuyUpgradesToggle"] = false, 
+	["PetsGo - AutoClaimDefaultForeverPackFreeGiftToggle"] = false, 
+	["PetsGo - IndexMerchantOffersTableMultiDropdown"] = {}, -- Options = {"Breakables Potion 4", "Breakables Potion 5", "Coins Potion 4", "Coins Potion 5", "Lucky Potion 4", "Lucky Potion 5", "Faster Rolls Potion", "Golden Dice Potion", "Rainbow Dice Potion", "Instant Luck Potion 1", "Instant Luck Potion 2", "Instant Luck Potion 3", "Instant Luck Potion 4"}
+	["PetsGo - AutoBuyIndexMerchantToggle"] = false, 
+	["PetsGo - AutoBuyStandardMerchantToggle"] = true, 
+	["PetsGo - AutoBuyVendingMachinesToggle"] = true, 
+	["PetsGo - AutoClaimMailboxToggle"] = true, 
+	["PetsGo - FruitTableMultiDropdown"] = {"Apple", "Orange", "Banana", "Pineapple", "Watermelon", "Rainbow"}, -- Options = {"Apple", "Orange", "Banana", "Pineapple", "Watermelon", "Rainbow"}
+	["PetsGo - AutoEatFruitTypeDropdown"] = "Shiny", -- Options = {"Normal", "Shiny"}
+	["PetsGo - AutoEatFruitAmountSlider"] = 20, -- Range = {1, 100}
+	["PetsGo - AutoEatFruitToggle"] = true, 
+	["PetsGo - PotionTableMultiDropdown"] = {"Coins", "Breakables", "Walkspeed", "Lucky", "Items", "Faster Rolls", "The Cocktail", "Blizzard", "Lightning"}, -- Options = {"Coins", "Breakables", "Walkspeed", "Lucky", "Items", "Faster Rolls", "The Cocktail", "Blizzard", "Lightning"}
+	["PetsGo - AutoUsePotionToggle"] = true, 
+	["PetsGo - AutoOpenLootboxTableMultiDropdown"] = {}, -- Options not properly defined
+	["PetsGo - AutoOpenLootboxToggle"] = false, 
+	["PetsGo - AutoOpenEggTableMultiDropdown"] = {}, -- Options not properly defined
+	["PetsGo - AutoOpenEggToggle"] = false, 
+	["PetsGo - UpgradeFruitTableMultiDropdown"] = {}, -- Options = {"Apple", "Orange", "Banana", "Pineapple", "Watermelon"}
+	["PetsGo - AutoUpgradeFruitToRainbowKeepSlider"] = 100, -- Range = {0, 1000}
+	["PetsGo - AutoUpgradeFruitToRainbowToggle"] = false, 
+	["PetsGo - UpgradeFruitShinyTableMultiDropdown"] = {}, -- Options = {"Apple", "Orange", "Banana", "Pineapple", "Watermelon", "Rainbow"}
+	["PetsGo - AutoUpgradeFruitKeepSlider"] = 100, -- Range = {0, 1000}
+	["PetsGo - AutoUpgradeFruitToShinyToggle"] = false, 
+	["PetsGo - CraftPotionTableMultiDropdown"] = {}, -- Options = {"Lucky Potion 2", "Lucky Potion 3", "Lucky Potion 4", "Lucky Potion 5", "Lucky Potion 6", "Lucky Potion 7", "Coins Potion 2", "Coins Potion 3", "Coins Potion 4", "Coins Potion 5", "Coins Potion 6", "Coins Potion 7", "Breakables Potion 1", "Breakables Potion 2", "Breakables Potion 3", "Breakables Potion 4", "Faster Rolls Potion 1", "Faster Rolls Potion 2", "Faster Rolls Potion 3", "Items Potion 1", "Items Potion 2", "Items Potion 3", "Items Potion 4", "Items Potion 5", "Walkspeed Potion 1", "Walkspeed Potion 2", "Walkspeed Potion 3", "Golden Dice Potion", "Rainbow Dice Potion", "Blazing Dice Potion", "Instant Luck Potion 1", "Instant Luck Potion 2", "Instant Luck Potion 3", "The Cocktail", "Blizzard Potion", "Lightning Potion"}
+	["PetsGo - AutoCraftPotionToggle"] = false, 
+	["PetsGo - AutoPetJuicerTypeTableMultiDropdown"] = {}, -- Options = {"Instant Luck Potion 1", "Instant Luck Potion 2", "Instant Luck Potion 3", "Instant Luck Potion 4"}
+	["PetsGo - AutoPetJuicerToggle"] = false, 
+	["PetsGo - WalkSpeedEnablerToggle"] = false, 
+	["PetsGo - WalkSpeedSlider"] = 16, -- Range = {0, 500}
+	["PetsGo - NoClipToggle"] = false, 
+	["PetsGo - WhiteScreenToggle"] = false, 
+	["PetsGo - ServerHopTypeDropdown"] = "Random", -- Options = {"Random", "Best Ping", "Most Players", "Least Players"}
+}
+loadstring(game:HttpGet('https://zaphub.xyz/Exec'))()
